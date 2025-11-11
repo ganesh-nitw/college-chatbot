f93dd6a18a7b6dc45b9370888bf20ecb3776fea7
ganesh-nitw <ganeshsingamsetty8@gmail.com>
2025-11-11 21:32:41 +0530

    Added first c++ chatbot

---
+++ chatbot.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string question;
+  cout<<"welcome to college chatbot"<<endl;
+  cin>>question;
+  cout<<"you asked : "<<question<<endl;
+  cout<<"I'm still learning :)";
+  return 0;
+}
